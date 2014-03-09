a66f493e1329b3c344b2f38ce7aacf219635af0e
Sebastian Fröstl <sebastian@froestl.com>
2014-03-09 13:32:11 +0100

    Add CRUD REST Api for /activities

--- app.js
+++ app.js
@@ -1,49 +1,66 @@
+'use strict';
+
+// Models
+var TimeSession = require('./models/TimeSession.js');
+var Activity = require('./models/Activity.js');
+
+// Libs
 var express = require('express');
+var fs = require('fs');
 var q = require('q');
-var app = express();
 var MongoClient = require('mongodb').MongoClient, format = require('util').format;
 var ObjectID = require('mongodb').ObjectID;
 
-// Logger
-app.use(express.logger({}));
-// Body parser
-app.use(express.bodyParser({ limit: 1024 * 1024 }));
+// Logging dir
+var expressLogFile = fs.createWriteStream('./logs/express.log', {flags: 'a'});
 
+// Init and configure
+var app = express();
+app.configure(function(){
+  app.use(express.logger({stream: expressLogFile}));
+  app.use(express.bodyParser());
+});
 
 MongoClient.connect('mongodb://127.0.0.1:27017/timechicken', function (err, db) {
   if (err) { throw err; }
 
-  app.get('/', function(req, res) {
-    res.json({'api_status': 'live'});
+  app.get('/api', function(req, res) {
+    res.json({'status': 'live'});
   });
 
-  app.get('/tests', function (req, res) {
-    var a = db.collection('test_insert').find();
+  // Activities
+  app.get('/api/activities', function (req, res) {
+    var a = db.collection('activity').find();
     q.ninvoke(a, 'toArray').then(function (items) {
-      console.log('Found: ', items);
       res.json(items);
     });
   });
 
-  app.get('/tests/:id', function(req, res) {
+  app.get('/api/activities/:id', function(req, res) {
     var id = ObjectID(req.params.id);
-    var collection = db.collection('test_insert');
-    q.ninvoke(collection, 'findOne', id).then(function (object) {
+    q.ninvoke(db.collection('activity'), 'findOne', id).then(function (object) {
       res.json(object);
     });
   });
 
-  app.post('/tests', function (req, res) {
-    var data = req.body;
-    var collection = db.collection('test_insert');
-    q.ninvoke(collection, 'insert', data).then(function (objects) {
+  app.post('/api/activities', function (req, res) {
+    var data;
+    if (Array.isArray(req.body)) {
+      data = [];
+      req.body.forEach(function (item) {
+        data.push(new Activity(item));
+      });
+    } else {
+      data = new Activity(req.body);
+    }
+    q.ninvoke(db.collection('activity'), 'insert', data).then(function (objects) {
       res.json(objects);
     });
   });
 
-  app.del('/tests/:id', function (req, res) {
+  app.del('/api/activities/:id', function (req, res) {
     var id = ObjectID(req.params.id);
-    q.ninvoke(db.collection('test_insert'), 'remove', {_id: id }).then(function () {
+    q.ninvoke(db.collection('activity'), 'remove', {_id: id }).then(function () {
       res.send(204);
     });
   });
